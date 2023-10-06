5883b28c7661e771e9c22296924ca50e4c77c80a
Ostap8943 <ostap5673@gmail.com>
2023-10-06 10:59:41 +0300

    Виправлено!

--- main.js
+++ main.js
@@ -10,11 +10,12 @@ fs.readFile('data.json', 'utf8', (err, data) => {
 
     const jsonData = JSON.parse(data);
 
-    jsonData.sort((a, b) => a.value - b.value);
-
-    const filteredData = jsonData
-      .filter((x) => x.ku === '13' && parseFloat(x.value) > 5)
-      .map((x) => x.value.toString());
+   const filteredData = [];
+for (const x of jsonData) {
+  if (x.ku === '13' && parseFloat(x.value) > 5) {
+    filteredData.push(x.value.toString());
+  }
+}
 
     fs.writeFile('output.txt', filteredData.join('\n'), 'utf8', (err) => {
       if (err) {
